dd10db93c5b90fa1c9a324e9fd8c3ff78931b5a6
Sathla7832 <yc.sathla.chang@gmail.com>
2026-07-18 22:22:14 +0800

    Add member Google Calendar scheduling

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,2 +1,2 @@
 window.CORE_LAB_FIREBASE_CONFIG = {"apiKey":"AIzaSyDmn6j6OEqcPqX9Z4AFx1OTtQO8X38NhII","authDomain":"core-lab-member-portal.firebaseapp.com","projectId":"core-lab-member-portal","storageBucket":"core-lab-member-portal.firebasestorage.app","messagingSenderId":"441864360893","appId":"1:441864360893:web:30bb3643e57b6b46fab8d4"};
-window.CORE_LAB_BOOTSTRAP_ADMIN = "ychungchang@o365.fcu.edu.tw";
+window.CORE_LAB_BOOTSTRAP_ADMIN = "corelabfcu@gmail.com";
